27b983f7813a2ebeddccb3ab11c51c90237b241f
Andrea Marchesini <amarchesini@mozilla.com>
2021-11-08 15:19:06 +0100

    Comments

---
+++ src/comment.js
@@ -0,0 +1,55 @@
+import execa from 'execa';
+import inquirer from 'inquirer';
+import fs from 'fs';
+import temp from 'temp';
+
+import Command from './command.js';
+import Jira from './jira.js';
+import ErrorHandler from './errorhandler.js';
+
+class Comment extends Command {
+  addOptions(program) {
+    const commentCmd = program.command('comment')
+      .description('Add a comment to an issue')
+      .argument('<id>', 'The issue ID')
+      .action(async id => {
+        temp.track();
+
+        let file;
+        try {
+          file = temp.openSync('jira');
+        } catch (e) {
+          console.log("Failed to open a temporary file");
+          return;
+        }
+
+        const code = await new Promise(resolve => {
+          const subprocess = execa(process.env.EDITOR, [file.path], {
+            detached: true,
+            stdio: 'inherit',
+          });
+
+          subprocess.on('error', () => resolve(-1));
+          subprocess.on('close', resolve);
+        });
+
+        if (code !== 0) {
+          console.log("Failed to run the app");
+          return;
+        }
+
+        let comment = fs.readFileSync(file.path);
+        comment = comment.toString().trim();
+        if (comment === "") {
+          console.log("No comment message");
+          return;
+        }
+
+        const jira = new Jira(program);
+
+        const data = await jira.spin('Adding the comment...', jira.api.addComment(id, comment));
+      });
+  }
+};
+
+export default Comment;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import program from 'commander';
 import os from 'os';
 import path from 'path';
 
+import Comment from './comment.js';
 import Create from './create.js';
 import Init from './init.js';
 import Issue from './issue.js';
+import Preset from './preset.js';
 import Project from './project.js';
 import Query from './query.js';
-import Preset from './preset.js';
 import Run from './run.js';
 import Set from './set.js';
 import Sprint from './sprint.js';
@@ -15,6 +16,7 @@ import Sprint from './sprint.js';
 const DEFAULT_CONFIG_FILE = path.join(os.homedir(), ".jira.json")
 
 const commands = [
+  new Comment(),
   new Create(),
   new Init(),
   new Issue(),
